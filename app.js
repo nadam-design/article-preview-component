636a88ff8ea2abfbf32a0e361ef217683b0314d9
nadam-design <nagy.adam.gfx@gmail.com>
2025-01-09 14:54:48 +0100

    ARIA attribute fix

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const cardTooltip = document.querySelector('.card__tooltip');
 
 cardButton.addEventListener('click', () => {
     if (cardTooltip) {
+
         cardTooltip.classList.toggle('active');
+
+        const isTooltipVisible = cardTooltip.classList.contains('active');
+        cardTooltip.setAttribute('aria-hidden', !isTooltipVisible);
+        cardButton.setAttribute('aria-expanded', isTooltipVisible);
     }
+
     cardButton.classList.toggle('active');
 });
